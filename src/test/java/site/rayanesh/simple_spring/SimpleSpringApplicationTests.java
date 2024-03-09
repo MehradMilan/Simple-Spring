134d4be4521a0692cbbe68ac2c2b5bc1e396cd61
Mehrad <mehrad_milanloo@yahoo.com>
2024-03-09 12:59:46 +0330

    Init: simple calc api

---
+++ src/test/java/site/rayanesh/simple_spring/SimpleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package site.rayanesh.simple_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
